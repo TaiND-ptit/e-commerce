3a994252eb6ac25429aa08024207d9d6e86fb280
TaiND19 <123nguyenduytai@gmail.com>
2023-06-27 22:06:14 +0700

    handler error

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
 const express = require("express");
+const  dbConnect = require('./config/dbconnect')
+const initRoutes = require('./routes')
+
+
 require("dotenv").config();
+
 
 const app = express();
 const port = process.env.PORT || 8888;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use("/", (req, res) => {
-  res.send("server onnn");
-});
+dbConnect()
+initRoutes(app)
 
 app.listen(port, () => {
   console.log("server on port " + port);
